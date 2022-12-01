f9a62c4462e4b41c0d8750d6bb0a36df64c04946
ricardopontesdantas <ricardopontesdantas@gmail.com>
2022-12-01 06:20:49 -0300

    formulario de produtos

--- js/products.js
+++ js/products.js
@@ -2,13 +2,17 @@ const Products = {
     init: function() {
         this.cacheSelectors()
         this.buildProducts()
+        this.bindEvents()
     },
 
     cacheSelectors: function() {
         this.$list = document.querySelector('[data-js="products-list"]')
+        this.$form = document.querySelector('#form-products')
     },
 
-    bindEvents: function() {},
+    bindEvents: function() {
+        this.$form.addEventListener('submit', this.Events.form_submit)
+    },
 
     buildProducts: async function() {
         const response = await fetch('http://localhost:3000/api/products')
@@ -23,7 +27,33 @@ const Products = {
         this.$list.innerHTML = productsHTML
     },
 
-    Events: {}
+    Events: {
+        form_submit: function(event) {
+            event.preventDefault()
+
+            const name = this.name.value
+            const price = Number(this.price.value)
+
+            const product = { name, price }
+
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(product)
+            }
+
+            fetch('http://localhost:3000/api/products', options)
+                .then(response => response.json())
+                .then(data => {
+                    alert(data.message)
+                    this.reset()
+                    Products.buildProducts()
+                })
+                .catch(console.error)
+        },
+    }
 }
 
 Products.init()
